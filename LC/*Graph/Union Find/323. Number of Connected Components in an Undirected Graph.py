05258ad0823d209579cb1bccc34a84dd58f2a369
Lotte W <xiw178@pitt.edu>
2022-08-27 15:47:25 -0400

    8.26 night

---
+++ LC/*Graph/Union Find/323. Number of Connected Components in an Undirected Graph.py
@@ -0,0 +1,17 @@
+class Solution:
+    def countComponents(self, n: int, edges: List[List[int]]) -> int:
+        parent = list(range(n))
+
+        def find(x):
+            if parent[x] != x:
+                parent[x] = find(parent[x])
+            return parent[x]
+
+        def union(x, y):
+            rx, ry = find(x), find(y)
+            if rx != ry:
+                parent[rx] = ry
+
+        for x, y in edges:
+            union(x, y)
+        return len({find(i) for i in range(n)})
